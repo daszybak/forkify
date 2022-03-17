b7fd6c90a783a35b70c915ba705b6bc906873fa0
daszybak <karlo1diidd@gmail.com>
2022-03-17 17:29:20 +0100

    rokanje mvca

---
+++ src/js/model.js
@@ -0,0 +1,41 @@
+import { async } from 'regenerator-runtime';
+
+export const state = {
+  recipe: {},
+};
+
+const initGET = Object.freeze({
+  method: 'GET',
+  withCredentials: true,
+  headers: {
+    'X-Auth-Token': '5041de20-ab64-48dc-83d3-ba5eafd4bcc3',
+  },
+});
+
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`,
+      initGET
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    const recipe = structuredClone(data.data.recipe);
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(state.recipe);
+  } catch (err) {
+    console.log(err);
+  }
+};
